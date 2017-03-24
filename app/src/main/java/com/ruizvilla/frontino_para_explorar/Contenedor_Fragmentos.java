fa822be87976941f262cdd44a143a45fa9f0a9b5
SamuelRuiz <samuel.ruiz@udea.edu.co>
2017-03-24 09:44:40 +0100

    V2.2_Fragmentos_Dinamicos

---
+++ app/src/main/java/com/ruizvilla/frontino_para_explorar/Contenedor_Fragmentos.java
@@ -0,0 +1,13 @@
+package com.ruizvilla.frontino_para_explorar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Contenedor_Fragmentos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contenedor__fragmentos);
+    }
+}
